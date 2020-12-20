ed8fae142bed8efb4c2581bd204d3c203adbab8c
masato1983 <50264750+masato1983@users.noreply.github.com>
2020-12-20 10:48:15 +0900

    build(scss): implement sass sass-loader css-loader style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,21 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   devtool: 'eval-source-map',
-  entry: {
-    app: './src/app.js',
-    sub: './src/sub.js'
-  },
+  entry: { app: './src/app.js'},
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          `sass-loader`
+        ]
+      }
+    ]
   }
 }
