7af1458787d8e956aec80a075e818be41a60f283
masato1983 <50264750+masato1983@users.noreply.github.com>
2020-12-22 18:50:49 +0900

    build(html): implement html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -48,6 +49,10 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin({
       filename: '[name].[chunkhash].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body'
     })
   ]
 }
