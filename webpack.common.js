1249fe91afa8bbe5b558e53dc0f628ab168e8f0b
masato1983 <50264750+masato1983@users.noreply.github.com>
2020-12-23 09:50:22 +0900

    build(webpack.config.js): separate production and development config-file

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,11 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  devtool: 'eval-source-map',
+module.exports = ({outputFile, assetFile}) => ({
   entry: { app: './src/app.js', sub: './src/sub.js'},
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: '[name].[chunkhash].js'
+    filename: `${outputFile}.js`
   },
   module: {
     rules: [
@@ -39,7 +36,7 @@ module.exports = {
         test: /\.(jpe?g|gif|png|svg|woff2?|ttf|eot)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[contenthash].[ext]',
+          name: `${assetFile}.[ext]`,
           outputPath: 'images',
           publicPath: 'images'
         },
@@ -52,11 +49,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].[chunkhash].css'
+      filename: `${outputFile}.css`
     }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: 'body'
-    })
   ]
-}
+});
