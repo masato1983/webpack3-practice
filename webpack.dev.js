8dfd46d54f807cb5d80e308cec753d8f5b961638
masato1983 <50264750+masato1983@users.noreply.github.com>
2020-12-23 13:35:54 +0900

    build(package.json): add scripts dev & prod

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpackMerge = require('webpack-merge');
-const commonConf = require('webpack.common');
+const { merge } = require('webpack-merge');
+const commonConf = require('./webpack.common');
 const outputFile = '[name]';
 const assetFile = '[name]';
 
-module.exports = () => webpackMerge(commonConf({outputFile, assetFile}), {
+module.exports = () => merge(commonConf({outputFile, assetFile}), {
   mode: 'development',
-  devtool: 'eval-source-map',
+  devtool: 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
